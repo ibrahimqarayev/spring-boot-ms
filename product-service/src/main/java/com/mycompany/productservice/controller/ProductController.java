b0b8e66d960a3ff1eddd49c86833a1a5e35efb52
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-08 20:41:42 +0400

    Added Product Controller

---
+++ product-service/src/main/java/com/mycompany/productservice/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.mycompany.productservice.controller;
+
+import com.mycompany.productservice.dto.request.ProductRequest;
+import com.mycompany.productservice.dto.response.ProductResponse;
+import com.mycompany.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/product")
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+
+}
