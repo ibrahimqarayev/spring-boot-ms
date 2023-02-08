7277fe7acb7bd45d9123118051ea62589b9c8ecb
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-08 20:42:30 +0400

    Added Product

--- product-service/src/main/java/com/mycompany/productservice/model/Product.java
+++ product-service/src/main/java/com/mycompany/productservice/model/Product.java
@@ -1,4 +1,25 @@
 package com.mycompany.productservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product")
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
 }
