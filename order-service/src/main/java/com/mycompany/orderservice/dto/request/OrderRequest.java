42c7423f7941c07faeca6904495b5eca6db50ea5
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-09 22:42:51 +0400

    Added OrderRequest

---
+++ order-service/src/main/java/com/mycompany/orderservice/dto/request/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.mycompany.orderservice.dto.request;
+
+import com.mycompany.orderservice.dto.OrderLineItemsDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDtoList;
+}
