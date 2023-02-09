f105dba9075bc954557a3017971b24c5ada23f44
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-09 22:41:49 +0400

    Added Order

---
+++ order-service/src/main/java/com/mycompany/orderservice/model/Order.java
@@ -0,0 +1,25 @@
+package com.mycompany.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsLong;
+
+}
