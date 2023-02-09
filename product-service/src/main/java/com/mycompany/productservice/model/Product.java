ea8092c411cf9195a7e406c6371da396f395d68e
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-09 22:42:03 +0400

    Added Product

--- product-service/src/main/java/com/mycompany/productservice/model/Product.java
+++ product-service/src/main/java/com/mycompany/productservice/model/Product.java
@@ -13,10 +13,8 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "product")
-@Entity
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
