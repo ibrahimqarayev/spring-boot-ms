9cb613ed38a4f79c30209c15d04e21eabd34ece4
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-10 23:19:37 +0400

    Added OrderController

--- order-service/src/main/java/com/mycompany/orderservice/controlller/OrderController.java
+++ order-service/src/main/java/com/mycompany/orderservice/controlller/OrderController.java
@@ -3,6 +3,7 @@ package com.mycompany.orderservice.controlller;
 import com.mycompany.orderservice.dto.request.OrderRequest;
 import com.mycompany.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +13,9 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
         return "Order Placed Successfully !";
     }
 
