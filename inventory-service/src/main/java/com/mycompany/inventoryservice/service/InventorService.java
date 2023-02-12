4d1146ad45396cee2ed453a2dd9551d92cf59be9
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-12 23:45:55 +0400

    Added InventorService

--- inventory-service/src/main/java/com/mycompany/inventoryservice/service/InventorService.java
+++ inventory-service/src/main/java/com/mycompany/inventoryservice/service/InventorService.java
@@ -8,4 +8,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class InventorService {
     private final InventorRepository inventorRepository;
+
+    public boolean isInStock(String skuCode) {
+
+    }
 }
