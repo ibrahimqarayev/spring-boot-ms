821415f076157eafe05059df585b604a02a893b0
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-12 22:09:09 +0400

    Added ProductRepository

--- product-service/src/main/java/com/mycompany/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/mycompany/productservice/repository/ProductRepository.java
@@ -1,9 +1,8 @@
 package com.mycompany.productservice.repository;
 
+
 import com.mycompany.productservice.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
