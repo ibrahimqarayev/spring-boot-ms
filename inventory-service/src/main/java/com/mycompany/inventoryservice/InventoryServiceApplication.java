2eb8122299763bd11250b75631cedf7004c6af13
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-13 22:07:42 +0400

    Added CommandLineRunner

--- inventory-service/src/main/java/com/mycompany/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/mycompany/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,34 @@
 package com.mycompany.inventoryservice;
 
+import com.mycompany.inventoryservice.model.Inventor;
+import com.mycompany.inventoryservice.repository.InventorRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner loadData(InventorRepository inventorRepository) {
+        return args -> {
+            Inventor inventor1 = new Inventor();
+            inventor1.setSkuCode("Iphone_13");
+            inventor1.setQuantity(100);
+
+            Inventor inventor2 = new Inventor();
+            inventor2.setSkuCode("Iphone_13 red");
+            inventor2.setQuantity(0);
+
+            inventorRepository.save(inventor1);
+            inventorRepository.save(inventor2);
+
+        };
+    }
 
 }
