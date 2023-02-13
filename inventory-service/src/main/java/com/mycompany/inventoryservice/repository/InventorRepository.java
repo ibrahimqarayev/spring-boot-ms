e652b47b24d7cf7ad89a316f96d3418cd7995bbe
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-13 22:07:05 +0400

    Added InventorRepository

--- inventory-service/src/main/java/com/mycompany/inventoryservice/repository/InventorRepository.java
+++ inventory-service/src/main/java/com/mycompany/inventoryservice/repository/InventorRepository.java
@@ -3,5 +3,8 @@ package com.mycompany.inventoryservice.repository;
 import com.mycompany.inventoryservice.model.Inventor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InventorRepository extends JpaRepository<Inventor, Long> {
+    Optional<Inventor> findBySkuCode(String skuCode);
 }
