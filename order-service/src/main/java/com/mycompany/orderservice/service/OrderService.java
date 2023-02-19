6945056b7848715c6e211caa77317e70ae767ac0
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-19 20:59:37 +0400

    Injected WebClient

--- order-service/src/main/java/com/mycompany/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/mycompany/orderservice/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderConverter orderConverter;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     @Transactional
     public void placeOrder(OrderRequest orderRequest) {
@@ -37,8 +37,8 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).collect(Collectors.toList());
 
         //Call inventory service and place order if product is in stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
