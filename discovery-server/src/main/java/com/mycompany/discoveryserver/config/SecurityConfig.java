02141b403a8c062ffe77199e18ff2f9820bae03a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-23 23:41:38 +0400

    Override some WebSecurityConfigurerAdapter methods

--- discovery-server/src/main/java/com/mycompany/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/mycompany/discoveryserver/config/SecurityConfig.java
@@ -1,7 +1,18 @@
 package com.mycompany.discoveryserver.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("eureka").password("password")
+                .authorities("USER");
+    }
+
+
 }
