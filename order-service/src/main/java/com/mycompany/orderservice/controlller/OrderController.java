80acf9cdce7f3a3ae9f9bdcdd0de28a67c56b029
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-03-05 23:07:49 +0400

    Added CircuitBreaker annotation and fallbackMethod

--- order-service/src/main/java/com/mycompany/orderservice/controlller/OrderController.java
+++ order-service/src/main/java/com/mycompany/orderservice/controlller/OrderController.java
@@ -2,6 +2,7 @@ package com.mycompany.orderservice.controlller;
 
 import com.mycompany.orderservice.dto.request.OrderRequest;
 import com.mycompany.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully !";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException exception) {
+        return "Something went wrong !";
     }
 
 }
