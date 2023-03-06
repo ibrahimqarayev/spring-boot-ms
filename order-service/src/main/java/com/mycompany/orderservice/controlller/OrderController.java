62d10f3cce0463fd7702266410869b9493e5a929
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-03-06 23:35:20 +0400

    Added TimeLimiter annotation

--- order-service/src/main/java/com/mycompany/orderservice/controlller/OrderController.java
+++ order-service/src/main/java/com/mycompany/orderservice/controlller/OrderController.java
@@ -3,6 +3,7 @@ package com.mycompany.orderservice.controlller;
 import com.mycompany.orderservice.dto.request.OrderRequest;
 import com.mycompany.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully !";
